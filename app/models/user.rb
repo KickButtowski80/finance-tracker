e3e36a293719974ab9ca05605fdabebcf36ff609
kickbuttowski59 <pazpaz22@yahoo.com>
2018-11-07 01:54:02 +0000

    updated views and navigation

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,12 @@ class User < ApplicationRecord
          has_many :user_stocks, :dependent => :destroy
          has_many :stocks, through: :user_stocks 
          
+         
+         def full_name             
+             return "#{first_name} #{last_name}".strip if first_name || last_name
+             "Anonymos"
+         end
+         
          def stock_already_added?(ticker_symbol)
            stock = Stock.find_by_ticker(ticker_symbol)
            return false unless stock
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
  
-  devise_for :users
+  devise_for :users 
   root to: 'welcome#index'
   get 'my_portfolio', to: "users#my_portfolio"  
   get 'search_stocks', to: "stocks#search"
