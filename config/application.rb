c1a6d9844b2df2eab41e9f3c567188d9ffe968cb
kickbuttowski59 <pazpaz22@yahoo.com>
2018-11-15 16:53:33 +0000

    working with time zone

--- config/application.rb
+++ config/application.rb
@@ -15,7 +15,7 @@ module Workspace
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
-    config.time_zone = 'Eastern Time (US & Canada)'
+    config.time_zone = "Pacific Time (US & Canada)"
     config.active_record.default_timezone = :local
     config.beginning_of_week = :sunday
   end
