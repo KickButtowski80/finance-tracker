d07fd342589b5cb93d1be50c4aa81b4aeddd01c4
kickbuttowski59 <pazpaz22@yahoo.com>
2018-11-15 00:42:17 +0000

    fixed time zone issue

--- config/application.rb
+++ config/application.rb
@@ -15,7 +15,7 @@ module Workspace
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
-    
+    config.time_zone = 'Eastern Time (US & Canada)'
     config.beginning_of_week = :sunday
   end
 end
