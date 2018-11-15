87a5a687175383477520e12500b08b7068156c6e
kickbuttowski59 <pazpaz22@yahoo.com>
2018-11-15 01:55:10 +0000

    better coloring for each day, for example, days are not in the month are colored gray

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,5 +3,17 @@ module ApplicationHelper
   def stock_by_date(date)      
     UserStock.includes(:user, :stock ).where("date(created_at) = ? and user_id = ? ", date , current_user.id)  
   end
- 
+  
+  def color_day(day)
+    start_date = (params[:start_month] || Date.today).to_date
+    if (day ==  Date.current )
+      "background-color:#face"
+    elsif (start_date.beginning_of_month..start_date.end_of_month).include?(day)
+      "background-color: #ffff"
+    else
+      "background-color: gray"
+    end
+    
+  end
+  
 end
