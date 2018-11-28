509ddd4daa147a1a5569e9c42be44c45c06f7c9f
kickbuttowski59 <pazpaz22@yahoo.com>
2018-11-28 19:19:42 +0000

    added time zone field to sign up and edit user forms

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,11 +2,18 @@ class ApplicationController < ActionController::Base
     
     before_action :authenticate_user!    
     before_action :configure_permitted_parameters , if: :devise_controller?
-  
+    around_action :set_time_zone, if: :current_user
+    
+    
+    private
+    
+      def set_time_zone(&block)
+        Time.use_zone(current_user.time_zone, &block)
+      end
   protected
   
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :time_zone])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :time_zone])
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_15_201743) do
+ActiveRecord::Schema.define(version: 2018_11_28_183416) do
 
   create_table "friendships", force: :cascade do |t|
     t.integer "user_id"
@@ -56,6 +56,7 @@ ActiveRecord::Schema.define(version: 2018_11_15_201743) do
     t.datetime "updated_at", null: false
     t.string "first_name"
     t.string "last_name"
+    t.string "time_zone", default: "UTC"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
